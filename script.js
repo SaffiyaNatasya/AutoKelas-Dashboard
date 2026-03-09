8b5f1454d91e0242cb504acf0eebe56ed35976a7
SaffiyaNatasya <bezzaddd9856@gmail.com>
2026-03-09 21:46:04 +0800

    Add files via upload
    update dashboard

--- script.js
+++ script.js
@@ -1,22 +1,123 @@
-function toggleDevice(button) {
-    if (button.innerText === "OFF") {
-        button.innerText = "ON";
-        button.classList.add("on");
-    } else {
-        button.innerText = "OFF";
-        button.classList.remove("on");
-    }
+const options={
+username:"sfftsya",
+password:"Tasya_1875"
 }
 
-function simulateScan(readerNumber) {
+const client=mqtt.connect("wss://f5ed2fda712540be846684a2587c2008.s1.eu.hivemq.cloud:8884/mqtt",options)
 
-    const students = [
-        "Ali Ahmad - 20231234",
-        "Siti Nur - 20235678",
-        "John Tan - 20239876"
-    ];
+client.on("connect",function(){
 
-    const randomStudent = students[Math.floor(Math.random() * students.length)];
+console.log("MQTT Connected")
 
-    document.getElementById("rfid" + readerNumber).innerText = randomStudent;
+client.subscribe("autokelas/+/+/status")
+client.subscribe("autokelas/attendance")
+
+})
+
+client.on("message",function(topic,message){
+
+let msg=message.toString()
+
+// Lamp Status
+if(topic==="autokelas/elampu1/status"){
+
+document.getElementById("lamp1text").innerText=msg
+
+let dot=document.getElementById("lamp1dot")
+
+if(msg==="ON"){
+dot.classList.remove("off")
+dot.classList.add("on")
+}else{
+dot.classList.remove("on")
+dot.classList.add("off")
+}
+
+}
+
+if(topic==="autokelas/elampu2/status"){
+
+document.getElementById("lamp2text").innerText=msg
+
+let dot=document.getElementById("lamp2dot")
+
+if(msg==="ON"){
+dot.classList.remove("off")
+dot.classList.add("on")
+}else{
+dot.classList.remove("on")
+dot.classList.add("off")
+}
+
+}
+
+if(topic==="autokelas/esoket1/status"){
+
+document.getElementById("soket1text").innerText=msg
+
+let dot=document.getElementById("soket1dot")
+
+if(msg==="ON"){
+dot.classList.remove("off")
+dot.classList.add("on")
+}else{
+dot.classList.remove("on")
+dot.classList.add("off")
+}
+
+}
+
+if(topic==="autokelas/esoket2/status"){
+
+document.getElementById("soket2text").innerText=msg
+
+let dot=document.getElementById("soket2dot")
+
+if(msg==="ON"){
+dot.classList.remove("off")
+dot.classList.add("on")
+}else{
+dot.classList.remove("on")
+dot.classList.add("off")
+}
+
+}
+
+// Attendance
+// Kehadiran 1
+if(topic==="autokelas/kehadiran1"){
+
+let data=JSON.parse(msg)
+
+let table=document.getElementById("attendance1")
+
+let row=table.insertRow(0)
+
+let name=row.insertCell(0)
+let date=row.insertCell(1)
+let time=row.insertCell(2)
+
+name.innerHTML=data.name
+date.innerHTML=data.date
+time.innerHTML=data.time
+
+}
+
+// Kehadiran 2
+if(topic==="autokelas/kehadiran2"){
+
+let data=JSON.parse(msg)
+
+let table=document.getElementById("attendance2")
+
+let row=table.insertRow(0)
+
+let name=row.insertCell(0)
+let date=row.insertCell(1)
+let time=row.insertCell(2)
+
+name.innerHTML=data.name
+date.innerHTML=data.date
+time.innerHTML=data.time
+
 }
